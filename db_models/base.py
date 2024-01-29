61fb66bc926b9f423f39c908dcce5da0addf033a
Spencer Jensen <roxasbeta@gmail.com>
2024-01-28 19:22:46 -0700

    Fixed DB Stuff

--- db_models/base.py
+++ db_models/base.py
@@ -1,4 +1,9 @@
-from sqlalchemy.ext.delcarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
+import sqlalchemy.ext.declarative as dec
 
 
-Base= declarative_base()
+class Base(DeclarativeBase):
+  pass
+
+
+SqlAlchemyBase = dec.declarative_base()
